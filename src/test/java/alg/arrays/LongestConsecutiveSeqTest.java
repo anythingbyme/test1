5c2fd965925666604f39e1b2a7825b72b357c7ff
Raj <libforme3@gmail.com>
2023-01-16 09:57:18 -0800

    Find longest seq (unit tests)

--- src/test/java/alg/arrays/LongestConsecutiveSeqTest.java
+++ src/test/java/alg/arrays/LongestConsecutiveSeqTest.java
@@ -19,4 +19,14 @@ Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefor
         int[] nums = {100,4,200,1,3,2};
         Assertions.assertEquals(4, lcs.longestConsecutive(nums));
     }
+
+    @Test
+    void longestConsecutive2() {
+        /*
+Input: nums = [0,3,7,2,5,8,4,6,0,1]
+Output: 9
+         */
+        int[] nums = {0,3,7,2,5,8,4,6,0,1};
+        Assertions.assertEquals(9, lcs.longestConsecutive(nums));
+    }
 }
