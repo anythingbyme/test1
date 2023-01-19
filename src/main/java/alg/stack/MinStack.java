5a0f9fb3f6cb97c4596ecdaa531bca4d6a119c4d
Raj <libforme3@gmail.com>
2023-01-19 13:26:01 -0800

    Min stack

---
+++ src/main/java/alg/stack/MinStack.java
@@ -0,0 +1,39 @@
+package alg.stack;
+
+import ds.stack.Stack;
+import lombok.Getter;
+
+@Getter
+public class MinStack {
+
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        this.minStack = new Stack<>();
+        this.stack = new Stack<>();
+
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.size() == 0 || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        int n = stack.pop();
+        if (n == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
---
+++ src/test/java/alg/stack/MinStackTest.java
@@ -0,0 +1,34 @@
+package alg.stack;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Stack;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MinStackTest {
+
+    @Test
+    void push() {
+    }
+
+    @Test
+    void pop() {
+    }
+
+    @Test
+    void top() {
+    }
+
+    @Test
+    void getMin() {
+    }
+
+    @Test
+    void push1() {
+        Stack<Integer> s = new Stack<>();
+        s.push(6);
+        s.push(6);
+        System.out.println(s.size());
+    }
+}
